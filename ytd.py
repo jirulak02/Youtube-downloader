7f99b2ed065f26d35333f3829c5acca606826197
jirulak02 <tchooorulok@gmail.com>
2023-01-22 11:58:28 +0100

    done

--- ytd.py
+++ ytd.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 from sys import argv
 
 link = argv[1]
+directory = argv[2]
 yt = YouTube(link)
 yd = yt.streams.get_highest_resolution()
 
@@ -11,8 +12,7 @@ yd = yt.streams.get_highest_resolution()
 from pytube import Playlist
 p = Playlist(link)
 for video in p.videos:
-    video.streams.get_highest_resolution().download('D:\Jirka\Movies\Videa')
+    video.streams.get_highest_resolution().download(directory)
 """
 
-# The directory where you want to download to
-yd.download('D:\Jirka\Movies\Videa')
+yd.download(directory)
